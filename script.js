b1e1aeb19d083ed8b0965cc1bc9e864148a1b117
Nick Ezell <alexsandratrammell@Nicks-MacBook-Pro.local>
2020-01-09 22:46:10 -0600

    Began defining character inclusion variables and creating password generation function.

--- script.js
+++ script.js
@@ -1,30 +1,74 @@
+
+
 //Arrays containing our characters
 
-const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", 
-"l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", 
+"l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
 
-const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
- "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+ upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
+ "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
 
-const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+ numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
 
-const specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "?"]
+ specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "?"]
 
-//Prompts to ascertain used character types
 
-let passLength = prompt('Enter (using a number between 8-128) your preferred password length.'), 
+//Prompt to ascertain passLength
+let passLength = prompt('Enter (using a number between 8-128) your preferred password length.') 
+//Changing passLength string to integer
+let parsedLength = parseInt(passLength)
+//Logging parsedLength for integrity
+console.log("Password length: " + parsedLength)
 
-    chooseLower = confirm("Include lowercase?"), 
+//Confirmations for used character types
+let chooseLower = confirm("Include lowercase?")     
+
+let chooseUpper = confirm("Include uppercase?")
     
-    chooseUpper = confirm("Include uppercase?"),
+let chooseNum = confirm ("Include numbers?")
     
-    chooseNum = confirm ("Include numbers?"),
-    
-    chooseSpecial = confirm("Include special characters?")
+let chooseSpecial = confirm("Include special characters?")
 
-//Loop for alphabetic characters
+//Variables for character inclusions
+var allChar = [];
 
-for(i = 0; i < 27; i++)
+if(chooseLower && chooseUpper && chooseNum && chooseSpecial) {
+    allChar = lowerCase.concat(upperCase, numbers, specialChars)
+}
+
+
+else if(chooseLower && chooseUpper && chooseNum) {
+    var noSpec = lowerCase.concat(upperCase, numbers)
+}
+
+else if(chooseLower && chooseUpper) {
+    var onlyAlph = lowerCase.concat(upperCase)
+}
+
+else if(chooseUpper && chooseNum && chooseSpecial) {
+    var noLower = upperCase.concat(numbers, specialChars)
+}
+
+
+
+//Loop for password generation
+
+function passGen(char) {
+    console.log(char)
+    var password = "";
+    for(i = 0; i < parsedLength; i++) {
+        var randomNum = Math.floor(Math.random() * char.length);
+        //line 61 needs to be captured in a string.
+        char[randomNum]
+        
+    }
+
+    // document.getElementById("textArea").value = password
+}
+
+passGen(allChar);
+
+
 
 
 
